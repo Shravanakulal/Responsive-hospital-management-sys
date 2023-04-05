40de17032c363db050b7c6b35395dc08ad36bf46
Sanaa <91124607+SanaaKittur@users.noreply.github.com>
2023-04-06 01:14:01 +0530

    added function of drop down

--- script.js
+++ script.js
@@ -10,13 +10,30 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
 function show_alert(){
-    alert("Apppointment Confirmed !");
+    alert("Appointment Confirmed !!");
 }
 
 
-
-  $(function() {
-    $( "#dialog" ).dialog();
-  });
+function dynamicdropdown(listindex)
+  {
+      switch (listindex)
+      {
+      case "HSR" :
+          document.getElementById("status").options[0]=new Option("Dr. Rajesh T","Dr. Rajesh T");
+          document.getElementById("status").options[1]=new Option("Dr. Shilpa","Dr. Shilpa");
+          document.getElementById("status").options[2]=new Option("Dr. Saanvi","Dr. Saanvi");
+          break;
+      case "BTM" :
+          document.getElementById("status").options[0]=new Option("Dr. Aditya","Dr. Aditya");
+          document.getElementById("status").options[1]=new Option("Dr. Pratham","Dr. Pratham");
+          document.getElementById("status").options[2]=new Option("Dr. Khurshida","Dr. Khurshida");
+          break;
+      case "KLG" :
+        document.getElementById("status").options[0]=new Option("Dr. Mustafa","Dr. Mustafa");
+        document.getElementById("status").options[1]=new Option("Dr. Priya","Dr. Priya");
+        document.getElementById("status").options[2]=new Option("Dr. Sandra","Dr. Sandra");
+        break;
+      }
+      return true;
+  }
